6ee873e40cbdd89cbfac063bd2199ab864f8e577
Friedrich Schwedler <friedrich@fjodor>
2014-02-28 10:05:30 +0100

    corect dimensions of picture added

--- xkcd-clock.rb
+++ xkcd-clock.rb
@@ -21,12 +21,14 @@ class Time
   end
 end
 
+dimension = {width: 1366, height: 768}
+
 time = Time.now
 options = {}
 options[:innerAngle] = time.toAngle
 
 OptionParser.new do |opts|
-  opts.on("-a N", "--angle=N", Float, "angle to rotate the map, calculatet from time by default (must be set first)") do |n|
+  opts.on("-a", "--angle=N", Float, "angle to rotate the map, calculatet from time by default (must be set first)") do |n|
     if options[:local] then
       options[:outerAngle] = n
     else
@@ -34,7 +36,7 @@ OptionParser.new do |opts|
     end
   end
 
-  opts.on("-z N", "--zone=N", Integer, "specifie timezone to be upside") do |n|
+  opts.on("-z", "--zone=N", Integer, "specifie timezone to be upside") do |n|
     options[:local] = true
     options[:outerAngle] = options[:innerAngle]
     options[:innerAngle] = n * 15
@@ -66,6 +68,6 @@ inside.rotate!(options[:innerAngle])
 inside.crop!(NorthWestGravity, width, height)
 
 outside.composite!(inside.transparent('white'), 0, 0, OverCompositeOp)
-outside.write("now.png")
-
-puts options
+now = Image.new(dimension[:width],dimension[:height])
+now.composite!(outside, CenterGravity, OverCompositeOp)
+now.write("now.png")
